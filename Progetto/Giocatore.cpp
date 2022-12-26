4e8519394746b792402dfe2f33d9d54eca195c4b
Francesco <francesco.roncolato@studenti.unipd.it>
2022-12-26 20:41:18 +0100

    Aggiunto Giocatore

---
+++ Progetto/Giocatore.cpp
@@ -0,0 +1,12 @@
+#include "Giocatore.h"
+
+Giocatore::Giocatore(std::string n)
+	: name{n}
+{
+	// attack e defence inizializzate di default
+}
+
+Giocatore::~Giocatore()
+{
+}
+
---
+++ Progetto/Giocatore.h
@@ -0,0 +1,21 @@
+// autore: 
+
+#ifndef GIOCATORE_H
+#define GIOCATORE_H
+
+#include "GrigliaAttacco.h"
+#include "GrigliaDifesa.h"
+
+class Giocatore
+{
+public:
+	Giocatore(std::string n = "Player");
+	
+	GrigliaAttacco attack;
+	GirgliaDifesa defence;
+	std::string name;
+	
+	~Giocatore();
+};
+
+#endif // GIOCATORE_H
